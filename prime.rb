e9536a6300163c1fc0a5418a67f28393eb616aa2
Kunal <kdshah6593@gmail.com>
2020-11-11 14:32:55 -0500

    commit tree c5b022b781ee68ad0d6bd0f93563e998f26dc3e3

--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
 # Add  code here!
+#cleaner version
+def prime?(num)
+    if num <= 1
+        return false
+    else
+        num_range = (2..num - 1).to_a
+        return false if num_range.any? {|x| num % x == 0 }
+        true
+    end
+end
+
+#original code
+# def prime?(num)
+#     if num <= 1
+#         return false
+#     else
+#         num_range = (1..num).to_a
+#         num_range.pop
+#         num_range.shift
+#         not_prime = num_range.any? {|x| num % x == 0 }
+
+#         if not_prime == true
+#             return false
+#         else
+#             return true
+#         end
+#     end
+# end
